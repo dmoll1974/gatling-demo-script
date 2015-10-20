d261dc509a8ac109b012554de5e7524da4750412
x090371 <Daniel.Moll@klm.com>
2015-10-20 10:36:44 +0200

    bla

--- src/test/scala/com/gatling/demo/gatling/helpers/Assertions.scala
+++ src/test/scala/com/gatling/demo/gatling/helpers/Assertions.scala
@@ -11,12 +11,31 @@ import io.gatling.http.Predef._
 class Assertions extends Simulation{
 
 
+  var targetsIoUrl = System.getProperty("targetsIoUrl")
+  var productName = System.getProperty("productName")
+  var dashboardName = System.getProperty("dashboardName")
+  var testRunId = System.getProperty("testRunId")
 
   val httpProtocol = http
-    .baseURL(System.getProperty("targetsIoUrl"))
+    .baseURL(targetsIoUrl)
     .extraInfoExtractor(ExtraInfo => {
     if (ExtraInfo.status == KO)
-      println("Failed assertion: " + ExtraInfo.request.getUri())
+      ExtraInfo.requestName match{
+
+        case "Get requirements results for test run" =>
+
+          println("Requirements results failed: " + targetsIoUrl + "/#!/requirements/" + productName + "/" + dashboardName + "/" + testRunId + "/failed")
+
+        case "Get benchmark to previous build results" =>
+
+          println("Benchmark to previous build results failed: " + targetsIoUrl + "/#!/benchmark-previous-build/" + productName + "/" + dashboardName + "/" + testRunId + "/failed")
+
+        case "Get benchmark to fixed baseline results" =>
+
+          println("Benchmark to previous build results failed: " + targetsIoUrl + "/#!/benchmark-fixed-baseline/" + productName + "/" + dashboardName + "/" + testRunId + "/failed")
+
+      }
+
       println("Response: " + ExtraInfo.response.body)
     Nil
   })
@@ -29,18 +48,31 @@ class Assertions extends Simulation{
 
 
   val targetsIoAssertions =
-    exec(session => session.set("productName", System.getProperty("productName"))
-                           .set("dashboardName", System.getProperty("dashboardName"))
-                           .set("testRunId", System.getProperty("testRunId"))
+    exec(session => session.set("productName", productName)
+                           .set("dashboardName", dashboardName)
+                           .set("testRunId", testRunId)
     )
-      .exec(http("Get benchmark results for test run")
+      .exec(http("Get requirements results for test run")
+      .get( """/testrun/${productName}/${dashboardName}/${testRunId}""" )
+      .headers(ltdashHeaders)
+//      .check(jsonPath("$.benchmarkResultPreviousOK").is("true"))
+//      .check(jsonPath("$.benchmarkResultFixedOK").is("true"))
+      .check(jsonPath("$.meetsRequirement").is("true"))
+     )
+      .exec(http("Get benchmark to previous build results")
       .get( """/testrun/${productName}/${dashboardName}/${testRunId}""" )
       .headers(ltdashHeaders)
       .check(jsonPath("$.benchmarkResultPreviousOK").is("true"))
+//      .check(jsonPath("$.benchmarkResultFixedOK").is("true"))
+//      .check(jsonPath("$.meetsRequirement").is("true"))
+      )
+      .exec(http("Get benchmark to fixed baseline results")
+      .get( """/testrun/${productName}/${dashboardName}/${testRunId}""" )
+      .headers(ltdashHeaders)
+//      .check(jsonPath("$.benchmarkResultPreviousOK").is("true"))
       .check(jsonPath("$.benchmarkResultFixedOK").is("true"))
-      .check(jsonPath("$.meetsRequirement").is("true"))
-     )
-
+//      .check(jsonPath("$.meetsRequirement").is("true"))
+      )
   val assertionsScenario = scenario("assertions")
     .exec(targetsIoAssertions)
 
