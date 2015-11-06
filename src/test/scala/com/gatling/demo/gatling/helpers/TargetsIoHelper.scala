42978e845050dd812017be4eddff1bd678e22128
dmoll1974 <dmoll1974@gmail.com>
2015-11-06 21:19:21 +0100

    removed baseline

--- src/test/scala/com/gatling/demo/gatling/helpers/TargetsIoHelper.scala
+++ src/test/scala/com/gatling/demo/gatling/helpers/TargetsIoHelper.scala
@@ -11,7 +11,6 @@ class TargetsIoHelper extends Simulation {
   var baselineBuild : String = _
   var targetsIoUrl : String = _
   var buildResultKey : String = _
-  if (System.getProperty("baselineBuild") != null) baselineBuild = System.getProperty("baselineBuild") else  baselineBuild = "none"
   if (System.getProperty("buildResultKey") != null) buildResultKey = System.getProperty("buildResultKey") else buildResultKey = "MANUAL_TEST"
   if (System.getProperty("targetsIoUrl") != null) targetsIoUrl = System.getProperty("targetsIoUrl") else targetsIoUrl = "http://dashboard.com"
   val dashboardName = System.getProperty("dashboardName")
@@ -19,13 +18,13 @@ class TargetsIoHelper extends Simulation {
   val testRunId = System.getProperty("testRunId")
 
   if (testRunId != "DEBUG") {
-    println("targetsIoUrl: " + targetsIoUrl + " testRunId: "+ testRunId + " productName: " + productName + " dashboardName: " + dashboardName + " baselineBuild: " + baselineBuild + " buildResultKey: " + buildResultKey)
-    require(targetsIoUrl != null && testRunId != null && productName != null && dashboardName != null && baselineBuild != null)
+    println("targetsIoUrl: " + targetsIoUrl + " testRunId: "+ testRunId + " productName: " + productName + " dashboardName: " + dashboardName + buildResultKey)
+    require(targetsIoUrl != null && testRunId != null && productName != null && dashboardName != null)
   }
   
   def beforeSimulation() {
     if (testRunId != "DEBUG")
-        TargetsIoClient.sendEvent(targetsIoUrl, "start", testRunId, baselineBuild, buildResultKey, dashboardName, productName)
+        TargetsIoClient.sendEvent(targetsIoUrl, "start", testRunId,  buildResultKey, dashboardName, productName)
 
   }
 
@@ -36,7 +35,7 @@ class TargetsIoHelper extends Simulation {
   def afterSimulation() {
 
     if (testRunId != "DEBUG")
-        TargetsIoClient.sendEvent(targetsIoUrl, "end", testRunId, baselineBuild, buildResultKey, dashboardName, productName)
+        TargetsIoClient.sendEvent(targetsIoUrl, "end", testRunId, buildResultKey, dashboardName, productName)
 
   }
 
