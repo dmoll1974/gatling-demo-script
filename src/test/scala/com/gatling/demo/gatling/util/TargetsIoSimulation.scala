7412d486dcf3e088540f96651b5e71088d09829f
Daniel Moll <dmoll1974@gmail.com>
2016-02-13 15:22:40 +0100

    added productRelease

--- src/test/scala/com/gatling/demo/gatling/util/TargetsIoSimulation.scala
+++ src/test/scala/com/gatling/demo/gatling/util/TargetsIoSimulation.scala
@@ -9,6 +9,8 @@ class TargetsIoSimulation extends Simulation {
 
   var targetsIoUrl : String = _
   var buildResultsUrl : String = _
+  var productRelease : String = _
+  if (System.getProperty("productRelease") != null) productRelease = System.getProperty("productRelease") else productRelease = ""
   if (System.getProperty("buildResultsUrl") != null) buildResultsUrl = System.getProperty("buildResultsUrl") else buildResultsUrl = "MANUAL_TEST"
   if (System.getProperty("targetsIoUrl") != null) targetsIoUrl = System.getProperty("targetsIoUrl") else targetsIoUrl = "http://dashboard.com"
   val dashboardName = System.getProperty("dashboardName")
@@ -16,13 +18,13 @@ class TargetsIoSimulation extends Simulation {
   val testRunId = System.getProperty("testRunId")
 
   if (testRunId != "DEBUG") {
-    println("targetsIoUrl: " + targetsIoUrl + " testRunId: "+ testRunId + " productName: " + productName + " dashboardName: " + dashboardName  + " buildResultsUrl: " + buildResultsUrl)
+    println("targetsIoUrl: " + targetsIoUrl + " testRunId: "+ testRunId + " productName: " + productName + " productRelease: " + productRelease +" dashboardName: " + dashboardName  + " buildResultsUrl: " + buildResultsUrl)
     require(targetsIoUrl != null && testRunId != null && productName != null && dashboardName != null )
   }
 
   def beforeSimulation() {
     if (testRunId != "DEBUG")
-        TargetsIoClient.sendTestRunEvent(targetsIoUrl, "start", testRunId,  buildResultsUrl, dashboardName, productName)
+        TargetsIoClient.sendTestRunEvent(targetsIoUrl, "start", testRunId,  buildResultsUrl, dashboardName, productName, productRelease)
 
   }
 
@@ -33,7 +35,7 @@ class TargetsIoSimulation extends Simulation {
   def afterSimulation() {
 
     if (testRunId != "DEBUG")
-        TargetsIoClient.sendTestRunEvent(targetsIoUrl, "end", testRunId, buildResultsUrl, dashboardName, productName)
+        TargetsIoClient.sendTestRunEvent(targetsIoUrl, "end", testRunId, buildResultsUrl, dashboardName, productName, productRelease)
 
   }
 
